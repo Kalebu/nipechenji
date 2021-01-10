39d9244c8ae22f2f13957fe1a5be790f139ebdc3
Kalebu Gwalugano <isaackeinstein@gmail.com>
2021-01-10 03:37:18 +0300

    fixed request bug

--- back-end/app.py
+++ back-end/app.py
@@ -38,83 +38,89 @@ def index():
     return {"hello": "Damn"}
 
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    user_data = request.get_json()
-    if user_data:
-        name = user_data.get("name")
-        phone = user_data.get("phone")
-        location = user_data.get("location")
-        role = user_data.get("role")
-        password = user_data.get("password")
-        role = get_bool_role(role)
+    if request.method == "POST":
+        user_data = request.get_json()
+        if user_data:
+            name = user_data.get("name")
+            phone = user_data.get("phone")
+            location = user_data.get("location")
+            role = user_data.get("role")
+            password = user_data.get("password")
+            role = get_bool_role(role)
 
-        if all(name, phone, location, role):
-            new_user = Users(
-                name=name,
-                phone=phone,
-                location=location,
-                is_provider=role,
-                password=password,
-            )
-            db.session.add(new_user)
-            db.session.commit()
-            return {"user_added": True}
+            if all(name, phone, location, role):
+                new_user = Users(
+                    name=name,
+                    phone=phone,
+                    location=location,
+                    is_provider=role,
+                    password=password,
+                )
+                db.session.add(new_user)
+                db.session.commit()
+                return {"user_added": True}
     return {"user_added": False}
 
 
-@app.route("/signin")
+@app.route("/signin", methods=["POST", "GET"])
 def signin():
-    signin_data = request.get_json()
-    if signin_data:
-        phone = signin_data.get("phone")
-        password = signin_data.get("password")
-        user_exists = Users.query.filter_by(phone=phone).first()
-        if user_exists.is_authenticated(password):
-            token = create_token_string()
-            user_exists.token = token
-            db.session.add(user_exists)
-            db.session.commit()
-            return {"Token": token}
+    if request.method == "POST":
+
+        signin_data = request.get_json()
+        if signin_data:
+            phone = signin_data.get("phone")
+            password = signin_data.get("password")
+            user_exists = Users.query.filter_by(phone=phone).first()
+            if user_exists.is_authenticated(password):
+                token = create_token_string()
+                user_exists.token = token
+                db.session.add(user_exists)
+                db.session.commit()
+                return {"Token": token}
     return {"response": False}
 
 
-@app.route("/add-consumer")
+@app.route("/add-consumer", methods=["GET", "POST"])
 def add_consumer():
-    if request.get_json():
-        consumer_data = request.get_json()
-        token = consumer_data.get("token")
-        if token:
-            user_with_token = Users.query.filter_by(token=token).first()
-            if user_with_token:
-                amount = consumer_data.get("amount")
-                location = consumer_data.get("location")
-                new_consumer = Consumer(
-                    user_id=user_with_token.user_id,
-                    phone=user_with_token.phone,
-                    amount=amount,
-                    location=location,
-                )
-                db.session.add(new_consumer)
-                db.session.commit()
-                return {"reponse": True}
+    if request.method == "POST":
+
+        if request.get_json():
+            consumer_data = request.get_json()
+            token = consumer_data.get("token")
+            if token:
+                user_with_token = Users.query.filter_by(token=token).first()
+                if user_with_token:
+                    amount = consumer_data.get("amount")
+                    location = consumer_data.get("location")
+                    new_consumer = Consumer(
+                        user_id=user_with_token.user_id,
+                        phone=user_with_token.phone,
+                        amount=amount,
+                        location=location,
+                    )
+                    db.session.add(new_consumer)
+                    db.session.commit()
+                    return {"reponse": True}
     return {"reponse": False}
 
 
-@app.route("/toa-chenji")
+@app.route("/toa-chenji", methods=["GET", "POST"])
 def add_provider():
-    if request.get_json():
-        provider_data = request.get_json()
-        token = provider_data.get("token")
-        if token:
-            user_with_token = Users.query.filter_by(token=token).first()
-            if user_with_token:
-                consumer_phone = provider_data.get("consumer_phone")
-                main_consumer = Users.query.filter_by(phone=consumer_phone).first()
-                main_consumer.amepeta_chenji = True
-                db.session.add(main_consumer)
-                db.session.commit()
-                return {"reponse": "Chenji Imetolewa"}
+    if request.method == "POST":
+        if request.get_json():
+            provider_data = request.get_json()
+            token = provider_data.get("token")
+            if token:
+                user_with_token = Users.query.filter_by(token=token).first()
+                if user_with_token:
+                    consumer_phone = provider_data.get("consumer_phone")
+                    main_consumer = Users.query.filter_by(phone=consumer_phone).first()
+                    main_consumer.amepeta_chenji = True
+                    db.session.add(main_consumer)
+                    db.session.commit()
+                    return {"reponse": "Chenji Imetolewa"}
     return {"response": "Could not process your request"}
 
 
